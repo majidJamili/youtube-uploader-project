6c6f4d6c2826f101d36fa7629b4d01229b232e7d
majidjamili <jamilimajid@gmail.com>
2022-12-15 13:33:49 +1000

    Studio basic fucntionality is fully done - it only saves

--- public/scripts/submit-task-form.js
+++ public/scripts/submit-task-form.js
@@ -1,3 +1,6 @@
+
+
+
 const paramsDB = video._id;
 var responses = [];
 function pickStatusClass(type){
@@ -47,23 +50,25 @@ function responseTaskUI(title, status, time) {
 
     //status containers
     const statusID = document.createElement('i');
-    console.log('status', status)
-    console.log('class', pickStatusClass(status))
+
+
     statusID.classList.add(pickStatusClass(status));
-
-
-
-
     statusID.classList.add('fa-solid');
     statusID.classList.add('fa-circle');
-
     taskDIV.appendChild(statusID);
-
     // Time Container
     const timeDIV = document.createElement('div');
     timeDIV.innerHTML = time;
     timeDIV.classList.add('task-time');
     taskDIV.appendChild(timeDIV);
+
+    // attached other data: 
+
+
+
+
+
+
 
     container.appendChild(taskDIV);
     updateDragList()
@@ -74,34 +79,124 @@ function responseTaskUI(title, status, time) {
 
 
 }
-// this function creates a task
-function createTask() {   
-    
+
+
+// create UI task with hidden input of all data needed
+function getDuration(start, end){
+ 
+    if(start < end){
+        duration = end - start
+    }else{
+        duration = 0
+    }
+    return duration; 
+}
+
+function createDataResponsebyList() {
+
+    //TASK CONTAINER UI
+    const container = document.getElementById('task-panel-container');
+    const taskDIV = document.createElement('div');
+    taskDIV.classList.add('draggable');
+    taskDIV.classList.add('task');
+    taskDIV.setAttribute('draggable', true);
+
+    //FORM 
     const form = document.getElementById('task_form_studio');
-    const elements = form.elements;
+    const elements = form.elements; 
+    
+    //CREATE  TITLE:
+    const title = document.createElement('input'); 
+    title.setAttribute('type' , 'hidden'); 
+    title.setAttribute('name' , 'title'); 
+    title.value = elements['task-title'].value.toUpperCase();
+    taskDIV.appendChild(title);
+    //CREATE  TIME:
+    const time = document.createElement('input'); 
+    time.setAttribute('type' , 'hidden');
+    time.setAttribute('name' , 'time'); 
     const startTime = elements['start-time-input'].value;
     const endTime = elements['end-time-input'].value;
-    const time = endTime - startTime; 
-    const title = elements['task-title'].value;
-    const types = elements['task-type'].value;
-    elements['video-link'].value = video.youtube_video_url + '&t=' + startTime + 's';
-    const youtube_video_url = elements['video-link'].value;
-    responseTaskUI(title, types, time)
-    cleanTaskForm()
-    //updateDragList()
+    time.value = getDuration(startTime, endTime); 
+    taskDIV.appendChild(time);
+
+    //CREATE  TYPES:
+     const types = document.createElement('input'); 
+     types.setAttribute('type' , 'hidden');
+     types.setAttribute('name' , 'types'); 
+     types.value = elements['task-type'].value;
+     taskDIV.appendChild(types);
+
+    //CREATE  youtube_video_url:
+    const youtube_video_url = document.createElement('input'); 
+    youtube_video_url.setAttribute('type' , 'hidden');
+    youtube_video_url.setAttribute('name' , 'youtube_video_url'); 
+    youtube_video_url.value = video.youtube_video_url + '&t=' + startTime + 's';
+    taskDIV.appendChild(youtube_video_url);
 
 
-    responses.push({
-        title: title,
-        time: time,
-        types: types,
-        youtube_video_url: youtube_video_url
-    })
-    //return (responses);
 
+
+         //TITLE UI
+         const titleUI = document.createElement('p');
+         titleUI.innerHTML = title.value; 
+         taskDIV.appendChild(titleUI);
+
+          //STATUS UI
+        const statusUI = document.createElement('i');
+        statusUI.classList.add(pickStatusClass(types.value));
+        statusUI.classList.add('fa-solid');
+        statusUI.classList.add('fa-circle');
+        taskDIV.appendChild(statusUI);
+        // TIME UI
+
+        const timeUI = document.createElement('div');
+        timeUI.innerHTML = time.value;
+        timeUI.classList.add('task-time');
+        taskDIV.appendChild(timeUI);
+    
+
+
+
+
+         container.appendChild(taskDIV); 
+         updateDragList()
+         cleanTaskForm()
+
+    
+    
+    
 }
+
+//Exctracting the hidden values from the available tasks on the list and make them
+//relocatbales
+
+function extractResponse(){
+
+    const container = document.getElementById('task-panel-container');
+    const availableTask = container.querySelectorAll('.task'); 
+    availableTask.forEach(task => {
+        const inputs = task.querySelectorAll('input');
+            var response = {
+                    title: inputs[0].value,
+                    time: inputs[1].value,
+                    types: inputs[2].value,
+                    youtube_video_url: inputs[3].value
+                }
+               
+                responses.push(response)
+
+        // console.log('each task', task)
+        
+    });
+    //emptyTaskContainer()    
+    console.log('all New Responses', responses)
+}
+
 // this sends all the create tasks to the server side: 
 async function submitResponse() {
+    extractResponse()
+    console.log('information to submit', responses)
     const rawResponse = await fetch('/tasks/add' ,
         {
         method: 'POST',
@@ -114,22 +209,38 @@ async function submitResponse() {
                                            data:responses})
 
       });
-      
       const content = await rawResponse.json();
+      console.log('reponse to DB')
       return content; 
-      console.log(content); 
 
 
 }
+function submitTasks(){
+    submitResponse()
+    emptyTaskContainer()
+}
+
+
+
 
 
 //this delete the task once dropping is done: 
 
+function emptyTaskContainer(){
+    const binContainer = document.getElementById('task-panel-container'); 
+    binContainer.innerHTML =''; 
+    console.log('this is the cleaning functin and reponse is: ', responses)
+
+    while(responses.length>0){
+        responses.pop()
+    }
+    console.log('responses after deletion: ', responses)
+
+}
 
 function emptyBin(){
     const binContainer = document.getElementById('task-panel-container-delete'); 
     binContainer.innerHTML =''; 
-    binContainer.classList.remove('delete-dragover-drop')
 }
 
 
